3f99180e8b268b232782e7cff26feee335fa0a06
Magatiger <80549916+Magatiger@users.noreply.github.com>
2025-05-17 20:00:05 -0700

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -15,17 +15,17 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 //VALORANT GAMING LAYER
 
 #define MIRYOKU_LAYER_VAL \
-&kp TAB,          &kp Q,             &kp W,             &kp E,             &kp R,             &u_to_U_BASE,      U_NA,              U_NA,              U_NA,              U_NA,            \
+U_LT(TAB,ESC),    &kp Q,             &kp W,             &kp E,             &kp R,             &u_to_U_BASE,      U_NA,              U_NA,              U_NA,              U_NA,            \
 &kp LSHFT,        &kp A,             &kp S,             &kp D,             &kp F,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
 &kp LCTRL,        &kp Z,             &kp X,             &kp C,             U_MT(N4, B),       U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
-U_NP,             U_NP,              U_LT(LALT, ESC),   U_LT(U_VTWO, V),   &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
+U_NP,             U_NP,              &kp LALT,          U_LT(U_VTWO, V),   &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
   
 //VALORANT GAMING LAYER 2
 
 #define MIRYOKU_LAYER_VTWO \
-&kp Y,             &kp U,              &kp I,              &kp O,            &kp CAPSLOCK,      U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
+&kp TILDE2,        &kp U,              &kp I,              &kp O,            &kp CAPSLOCK,      U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
 &kp H,             &kp J,              &kp K,              &kp L,            &kp N,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
-&kp M,             &kp Q ,             &kp Q,              &kp Y,            &kp V,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
+&kp F1,            &kp F2,             &kp F3,            &kp F4,            &kp F5,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
 U_NP,              U_NP,               &kp LALT,            &kp LCTRL,        &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
 
 // Direct custom layer access
