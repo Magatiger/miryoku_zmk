5ccb9766a6bfc0c4da4fcde9ab5a269ae757f859
Magatiger <80549916+Magatiger@users.noreply.github.com>
2025-05-27 14:29:31 -0700

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -16,7 +16,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 
 #define MIRYOKU_LAYER_VAL \
 &kp ESC,          &kp Q,             &kp W,                 &kp E,             &kp R,            &u_to_U_BASE,      U_NA,              U_NA,              U_NA,              U_NA,            \
-&kp LSHFT,        &kp A,             &kp S,                 &kp D,             &kp F,            &u_to_U_MINE,              U_NA,              U_NA,              U_NA,              U_NA,            \
+&kp LSHFT,        &kp A,             &kp S,                 &kp D,             &kp F,            U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
 &kp LCTRL,        &kp Z,             &kp X,                 &kp C,             &kp B,            U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
 U_NP,             U_NP,              U_LT(U_VTWO, SPACE),   &kp V,             &kp LALT,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
   
@@ -27,13 +27,6 @@ U_NP,             U_NP,              U_LT(U_VTWO, SPACE),   &kp V,             &
 &kp TAB,           &kp J,              &kp K,              &kp L,            &kp F11,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
 &kp F1,            &kp F2,             &kp F3,            &kp F4,            &kp F5,             U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
 U_NP,              U_NP,               &kp LALT,            &kp LCTRL,        &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
-
-#define MIRYOKU_LAYER_MINE \
-&kp ESC,          &kp Q,             &kp W,                 &kp E,             &kp R,            &u_to_U_BASE,      U_NA,              U_NA,              U_NA,              U_NA,            \
-&kp LSHFT,        &kp A,             &kp S,                 &kp D,             &kp F,            U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
-&kp LCTRL,        &kp Z,             &kp X,                 &kp C,             &kp B,            U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
-U_NP,             U_NP,              U_LT(U_VTWO, LALT),   &kp V,             &kp SPACE,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
- 
 
 // Direct custom layer access
 #define MIRYOKU_LAYOUTMAPPING_CORNE(\
@@ -61,8 +54,7 @@ U_NP,             U_NP,              U_LT(U_VTWO, LALT),   &kp V,             &k
     MIRYOKU_X(SYM,    "Sym") \
     MIRYOKU_X(FUN,    "Fun") \
     MIRYOKU_X(VAL,   "Valo1") \
-    MIRYOKU_X(VTWO,   "Valo2") \
-    MIRYOKU_X(MINE,   "Mine") 
+    MIRYOKU_X(VTWO,   "Valo2")
     
     #define MIRYOKU_LAYERMAPPING_VAL MIRYOKU_MAPPING
     #define MIRYOKU_LAYERMAPPING_VTWO MIRYOKU_MAPPING
@@ -79,4 +71,3 @@ U_NP,             U_NP,              U_LT(U_VTWO, LALT),   &kp V,             &k
     #define U_FUN    9
     #define U_VAL    10
     #define U_VTWO   11
-    #define U_MINE   12
