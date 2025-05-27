396c9b59bba3b3227894f6c28fa131575ab58af6
Magatiger <80549916+Magatiger@users.noreply.github.com>
2025-05-27 14:02:44 -0700

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -16,7 +16,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 
 #define MIRYOKU_LAYER_VAL \
 &kp ESC,          &kp Q,             &kp W,                 &kp E,             &kp R,            &u_to_U_BASE,      U_NA,              U_NA,              U_NA,              U_NA,            \
-&kp LSHFT,        &kp A,             &kp S,                 &kp D,             &kp F,            &u_to_U_Mine,              U_NA,              U_NA,              U_NA,              U_NA,            \
+&kp LSHFT,        &kp A,             &kp S,                 &kp D,             &kp F,            &u_to_U_MINE,              U_NA,              U_NA,              U_NA,              U_NA,            \
 &kp LCTRL,        &kp Z,             &kp X,                 &kp C,             &kp B,            U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            \
 U_NP,             U_NP,              U_LT(U_VTWO, SPACE),   &kp V,             &kp LALT,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP 
   
